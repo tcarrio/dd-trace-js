c2bccbd9fe0d80f49c70a4cab8189127c23e55f5
Bryan English <bryan.english@datadoghq.com>
2022-05-12 22:59:25 -0400

    faster bind WIP

--- packages/datadog-instrumentations/src/helpers/instrument.js
+++ packages/datadog-instrumentations/src/helpers/instrument.js
@@ -7,6 +7,7 @@ const Hook = require('./hook')
 const requirePackageJson = require('../../../dd-trace/src/require-package-json')
 const { AsyncResource } = require('async_hooks')
 const log = require('../../../dd-trace/src/log')
+const {prototype} = require('events')
 
 const pathSepExpr = new RegExp(`\\${path.sep}`, 'g')
 const channelMap = {}
@@ -51,43 +52,36 @@ function filename (name, file) {
   return [name, file].filter(val => val).join('/')
 }
 
-// AsyncResource.bind exists and binds `this` properly only from 17.8.0 and up.
-// https://nodejs.org/api/async_context.html#asyncresourcebindfn-thisarg
-if (semver.satisfies(process.versions.node, '>=17.8.0')) {
-  exports.AsyncResource = AsyncResource
-} else {
-  exports.AsyncResource = class extends AsyncResource {
-    static bind (fn, type, thisArg) {
-      type = type || fn.name
-      return (new exports.AsyncResource(type || 'bound-anonymous-fn')).bind(fn, thisArg)
-    }
-
-    bind (fn, thisArg) {
-      let bound
-      if (thisArg === undefined) {
-        const resource = this
-        bound = function (...args) {
-          args.unshift(fn, this)
-          return Reflect.apply(resource.runInAsyncScope, resource, args)
-        }
-      } else {
-        bound = this.runInAsyncScope.bind(this, fn, thisArg)
+class DDAsyncResource extends AsyncResource {
+  bind (fn, thisArg) {
+    let bound
+    if (thisArg === undefined) {
+      const resource = this
+      bound = function (...args) {
+        args.unshift(fn, this)
+        return Reflect.apply(resource.runInAsyncScope, resource, args)
       }
-      Object.defineProperties(bound, {
-        'length': {
-          configurable: true,
-          enumerable: false,
-          value: fn.length,
-          writable: false
-        },
-        'asyncResource': {
-          configurable: true,
-          enumerable: true,
-          value: this,
-          writable: true
-        }
-      })
-      return bound
+    } else {
+      bound = this.runInAsyncScope.bind(this, fn, thisArg)
     }
+    Object.defineProperty(bound, 'length', {
+      configurable: true,
+      enumerable: false,
+      value: fn.length,
+      writable: false
+    })
+    bound.asyncResource = this
+    return bound
   }
 }
+
+// AsyncResource.bind exists and binds `this` properly only from 17.8.0 and up.
+// https://nodejs.org/api/async_context.html#asyncresourcebindfn-thisarg
+if (!semver.satisfies(process.versions.node, '>=17.8.0')) {
+  DDAsyncResource.bind = function (fn, type, thisArg) {
+    type = type || fn.name
+    return (new exports.AsyncResource(type || 'bound-anonymous-fn')).bind(fn, thisArg)
+  }
+}
+
+exports.AsyncResource = DDAsyncResource
